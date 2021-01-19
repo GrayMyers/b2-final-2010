5f7675bd7a040e07cf4fc1d73739e426fe8e0c77
Grayson Myers <graymyers10@gmail.com>
2021-01-19 10:16:13 -0700

    Add test for player relation of competition model

--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -4,5 +4,6 @@ describe Competition, type: :model do
   describe "relations" do
     it {should have_many :competition_teams}
     it {should have_many(:teams).through(:competition_teams)}
+    it {should have_many(:players).through(:teams)}
   end
 end
