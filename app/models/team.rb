f8a329746b7164a116dd38e08f77a6cfdb8a7f1b
Grayson Myers <graymyers10@gmail.com>
2021-01-19 11:42:40 -0700

    Add comment

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,7 @@ class Team < ApplicationRecord
   has_many :competitions, through: :competition_teams
   has_many :players
 
-  def self.by_average_age
+  def self.by_average_age #this would ideally be done using scopes, but ran out of time to implement that
     Team.joins(:players).group(:id).select("teams.*, avg(players.age) as avg_age").order("avg_age DESC")
   end
 end
