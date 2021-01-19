4c807f2aeeb8bc37faa291a8dcff1442052ba4ef
Grayson Myers <graymyers10@gmail.com>
2021-01-19 10:18:33 -0700

    Fix more typos

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -9,7 +9,7 @@ describe Player, type: :model do
     it "average_age" do
       expect(Player.average_age).to eq(nil) #edge case (expect it not to error when called with no players)
 
-      @team1 = Team.create(nickname: "team 1", nickname: "somewhere")
+      @team1 = Team.create(nickname: "team 1", hometown: "somewhere")
       @player1 = Player.create(name: "player 1", age: 12, team: @team1)
       @player2 = Player.create(name: "player 2", age: 13, team: @team1)
       @player3 = Player.create(name: "player 3", age: 14, team: @team1)
@@ -17,7 +17,7 @@ describe Player, type: :model do
       expect(Player.average_age).to eq(13)
       @player5 = Player.create(name: "player 5", age: 16, team: @team1)
 
-      expect(Player.average_age).to sq(13.75) #edge case
+      expect(Player.average_age).to eq(13.75) #edge case
     end
   end
 end
