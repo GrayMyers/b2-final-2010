70819c9566a2c4824ed0cea62c3f54edd8cee9ab
Grayson Myers <graymyers10@gmail.com>
2021-01-19 09:21:13 -0700

    Add competition model and relations

--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe Comptetition, type: :model do
+describe Competition, type: :model do
   describe "relations" do
     it {should have_many :competition_teams}
     it {should have_many(:teams).through(:competition_teams)}
