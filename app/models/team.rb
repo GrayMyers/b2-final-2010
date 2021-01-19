0ebda99237cfd513e88a15b37946dcd563397baf
Grayson Myers <graymyers10@gmail.com>
2021-01-19 11:42:16 -0700

    add by_average_age method

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,8 @@ class Team < ApplicationRecord
   has_many :competition_teams
   has_many :competitions, through: :competition_teams
   has_many :players
+
+  def self.by_average_age
+    Team.joins(:players).group(:id).select("teams.*, avg(players.age) as avg_age").order("avg_age DESC")
+  end
 end
