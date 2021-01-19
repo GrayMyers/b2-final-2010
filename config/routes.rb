ee744b63cc35c0ae4d75f2c9d70cf3ea95466056
Grayson Myers <graymyers10@gmail.com>
2021-01-19 11:28:31 -0700

    Add teams index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :competitions, only: [:index, :show] do
-    resources :teams, only: [:new, :create]
+    resources :teams, only: [:new, :create, :index]
   end
 end
